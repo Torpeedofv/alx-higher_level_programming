0018e5e3d544fa9018c6b108098fa05934acf352
Torpeedofv <torpeedo.fv@gmail.com>
2022-12-05 03:07:12 -0800

    commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             if j is not i:
-                print(' ', end='')        
+                print(' ', end='')
             print('{:d}'.format(j), end='')
         print()
