6f97cb7df45a4c51642c62ba6ce94efefd9b2e52
Torpeedofv <torpeedo.fv@gmail.com>
2022-12-10 02:07:10 +0100

    commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i in sorted(a_dictionary):
-        print('{}: {}'.format(i, a_dictionary[i]*2))
+    return ({i: a_dictionary[i]*2 for i in a_dictionary})
