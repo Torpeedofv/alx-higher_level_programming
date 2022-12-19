067cadfdbb286a22954838c826b0cd7e0c4c4544
Torpeedofv <torpeedo.fv@gmail.com>
2022-12-19 22:45:22 +0100

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    for element in my_list:
-        if count == x:
+    counter = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+        except IndexError:
             break
-        count += 1 
-        print(element, end="")
-    print('')
-    return count 
+        else:
+            counter += 1
+    print()
+    return counter
+
