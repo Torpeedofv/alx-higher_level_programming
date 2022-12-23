ac1d926b969f983f88115c2cd0b4435bac1c8576
Torpeedofv <torpeedo.fv@gmail.com>
2022-12-23 23:12:12 +0100

    commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
+"""Define a class, Square."""
+
+
 class Square:
+    """represent a square."""
+
     def __init__(self, size=0):
+        """initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
+
     @property
     def size(self):
+        """get the size of the current square."""
         return self.__size
+
     @size.setter
     def size(self, value):
+        """Set the size of the square"""
         if type(value) != int:
             raise TypeError('size must be an integer')
         if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
-    def area(self):
-        return (self.__size)**2
 
+    def area(self):
+        """Calculate the current area of the square."""
+        return (self.__size)**2
