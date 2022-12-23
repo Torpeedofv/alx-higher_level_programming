88e7ce9d8a1367ea9329a395443052f34986c84b
Torpeedofv <torpeedo.fv@gmail.com>
2022-12-23 23:03:46 +0100

    commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
-        if type(size) != int:
+        self.size = size
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
             raise TypeError('size must be an integer')
-        if size < 0:
+        if value < 0:
             raise ValueError('size must be >= 0')
-        self.__size = size
+        self.__size = value
     def area(self):
-        return self.__size**2
+        return (self.__size)**2
 
