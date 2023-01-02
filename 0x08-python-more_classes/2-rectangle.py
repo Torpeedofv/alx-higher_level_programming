097aa31b6f75d0844e7118f26d25d8344a6cb592
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-02 23:34:41 +0100

    commit

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
     @property
     def height(self):
         """get the height of the rectangle"""
-        return self.__width
+        return self.__height
 
     @height.setter
     def height(self, value):
