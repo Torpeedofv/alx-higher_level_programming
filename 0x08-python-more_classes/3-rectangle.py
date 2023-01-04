1c2e883372864c0becc37850a791aba67d9373c0
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-05 00:19:12 +0100

    commit

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
     
     def __init__(self, width=0, height=0):
         """Initialize a new square"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -47,5 +47,13 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return 0
         return 2 * (self.__height + self.__width)
-
-
+    def __str__ (self):
+        """Prints rectangle with #"""
+        if self.__height == 0 or self.__width == 0:
+            return "" 
+        result = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                result += "#"
+            result += "\n"
+        return result
