c8ae220eb2069a2ca4d6e01585e6c4c23b217e47
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-09 22:19:30 +0100

    commit

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-
+base = __import__('7-base_geometry').BaseGeometry
 """A module for a child class Rectangle"""
 
 
-class Rectangle(BaseGeometry):
+class Rectangle(base):
 
     """creation of a class Rectangle"""
 
     def __init__(self, width, height):
-        self.integer_validator
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
