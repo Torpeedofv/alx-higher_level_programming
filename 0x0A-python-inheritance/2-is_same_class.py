c5e271201f91b4c003807fc7f75fe34321e61736
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-09 16:05:18 +0100

    commit

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""A Module for a function"""
+
+
+def is_same_class(obj, a_class):
+
+    """a function that checks if an object is an instance of a class"""
+
+    return type(obj) == a_class
