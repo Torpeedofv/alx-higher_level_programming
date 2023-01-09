d2a5fbf9dcca8e94e5b1c75a8d393d7d95c8b13e
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-09 16:14:40 +0100

    commit

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""A module for a function"""
+
+
+def is_kind_of_class(obj, a_class):
+
+    """A function that checks if an object is an instance of
+    a class or sub class"""
+
+    return isinstance(obj, a_class)
