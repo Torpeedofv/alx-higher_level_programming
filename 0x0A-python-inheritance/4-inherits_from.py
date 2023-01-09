bd7f84e40c3baaf2f8fc5a8332462366c1e9087e
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-09 17:23:47 +0100

    commit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""A module for a function"""
+
+
+def inherits_from(obj, a_class):
+
+    """A function that checks if an object is an instance of a class or super
+    class"""
+
+    return type(obj) != a_class
