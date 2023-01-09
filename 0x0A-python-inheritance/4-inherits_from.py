4d7ede58331c9368e20fc2a0c00ab1e3b231a052
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-09 17:26:17 +0100

    commit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ def inherits_from(obj, a_class):
     """A function that checks if an object is an instance of a class or super
     class"""
 
-    return type(obj) != a_class
+    return type(obj) != a_class and issubclass(type(obj), a_class)
