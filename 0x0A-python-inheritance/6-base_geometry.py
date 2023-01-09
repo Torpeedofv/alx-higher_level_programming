cecc3786a44e830560e971efad791ed8dc7e321a
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-09 17:36:23 +0100

    commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""A module for a class: BaseGeometry"""
+
+
+class BaseGeometry:
+
+    """the creation of a class BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
