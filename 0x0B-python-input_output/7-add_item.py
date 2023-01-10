5e39491fd01a1392468099a180705654f97ba4ee
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-10 19:33:17 +0100

    commit

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""A module for a script"""
+
+
+save = __import__('5-save_to_json_file').save_to_json_file
+
+load = __import__('6-load_from_json_file').load_from_json_file
+
+
+
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""A function module"""
+
+
+def class_to_json(obj):
+
+    """A function that returns the dictionary description with simple data\
+            structure"""
+
+    return obj.__dict__
