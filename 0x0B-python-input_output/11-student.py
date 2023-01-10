67f3c176533cf9209b54027a283326a4635b1a16
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-10 23:12:40 +0100

    commit

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-
-"""A class module"""
+"""student class"""
 
 
 class Student:
-
-    """creation of a class Student"""
-
+    """class stuff"""
     def __init__(self, first_name, last_name, age):
+        """initializes"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs is None:
-            return {key: value for key, value in self.__dict__.items()}
+        """prints __dict__"""
+        if(attrs and isinstance(attrs, list) and
+           all(isinstance(x, str) for x in attrs)):
+            return({x: y for x, y in self.__dict__.items() if x in attrs})
         else:
-            return {key: value for key, value in self.__dict__.item() if key in self}
-        def reload_from_json(self, json):
-            for key, value in json.items():
-                setattr(self, key, value)
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces allattributes with json"""
+        if(json):
+            self.__dict__ = json
