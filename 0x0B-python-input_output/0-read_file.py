4f509bdf5fe7df7c38e86d10ffe63fe5dc9a6cce
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-10 14:58:02 +0100

    commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""A module to read a file"""
+
+
+def read_file(filename=""):
+
+    """a function to read a file"""
+
+    with open(filename, encoding="utf-8") as filename:
+        for line in filename:
+            print(line, end="")
