4803b623c6c41d4a78d9994d5f05cc8d9f844dfe
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-10 15:49:28 +0100

    commit

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""A module for a function"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+
+    """A function that returns the JSON rep of an object"""
+
+    return json.dumps(my_obj)
