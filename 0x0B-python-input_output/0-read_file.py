120b9636d8a1f123cf5f1d9149b23338233389b6
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-10 15:13:59 +0100

    commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     """a function to read a file"""
 
     with open(filename, encoding="utf-8") as filename:
-        print(filename.read())
+        print(filename.read(), end="")
