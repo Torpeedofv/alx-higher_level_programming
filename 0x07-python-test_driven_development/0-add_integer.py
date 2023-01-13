574762eabea606a0e7fbc16114c119c0550f1568
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-13 20:50:52 +0100

    commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+"""A module for a function"""
+
+
 def add_integer(a, b=98):
-    if not isinstance(a, (float, int)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (float, int)):
-        raise TypeError("b must be an integer")
-    if isinstance(a, float) or isinstance(b, float):
+    """This is a function that adds to integers and returns the sum of the integers"""
+
+    if type(a) is float:
         a = int(a)
+
+    if type(b) is float:
         b = int(b)
-    return a + b
+
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+
+    return (a + b)
