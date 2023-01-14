5a30b85a6d0ecc678ea3e7d4450026e1d535725f
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-14 12:27:16 +0100

    commit

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,15 +4,16 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    firstList = len(matrix[0])
     newMat = []
     for i in matrix:
+        if len(i) != firstList:
+            raise TypeError("Each row of the matrix must have the same size")
         subMat = []
         for j in i:
             if not isinstance(j, (int, float)):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            multInt = j * 3
+            multInt = j / 3
             subMat.append(multInt)
-        if len(i) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
         newMat.append(subMat)
     return newMat
