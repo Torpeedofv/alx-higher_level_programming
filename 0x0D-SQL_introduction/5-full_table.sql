4188aa0e25a9f5e611618ce392789846faa4d3cd
Torpeedofv <torpeedo.fv@gmail.com>
2023-02-15 23:49:29 +0100

    commit

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- Prints the full description of the table from a database in a mysql server
+SHOW CREATE TABLE first_table;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- list all rows of the table from the database in mysql server
+SELECT * FROM first_table;
