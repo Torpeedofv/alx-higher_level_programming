743820400b4bd944b9b212963dd37051ebaaf932
Torpeedofv <torpeedo.fv@gmail.com>
2023-02-16 01:16:05 +0100

    commit

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- inserts a new row in the table in a database in mysql
+INSERT INTO first_table
+(`id`, `name`) VALUES (89, 'Best School');
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- a script that displays the number of records with a value in the table of the database in myssql
+SELECT COUNT(*) FROM first_table WHERE id=89;
