b3ec11234ad3c10e2685e0ecb82016039de5ec8a
Torpeedofv <torpeedo.fv@gmail.com>
2023-02-16 13:02:16 +0100

    commit

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- list the number of records with the same score in the table in mysql
-SELECT score, COUNT(*) FROM second_table GROUP BY score;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score DESC;
