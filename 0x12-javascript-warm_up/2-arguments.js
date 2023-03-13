4f0d0e3da71e5e7a3002301439b376c9128c0d68
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-13 13:35:02 +0100

    commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
+const argv = process.argv.slice(2);
 
 // print a message depending on the number of arguments passed
-if (argv.length <= 2) {
+if (argv.length === 0) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (argv.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-
-// print the first argument passed
-argv.forEach((val, index) => {
-  if (index === 2) {
-    console.log(val);
-  }
-  if (index === 1) {
-    console.log('No argument');
-  }
-});
+const arg = process.argv[2];
+if (typeof arg === 'undefined') {
+  console.log('No argument');
+} else {
+  console.log(arg);
+}
