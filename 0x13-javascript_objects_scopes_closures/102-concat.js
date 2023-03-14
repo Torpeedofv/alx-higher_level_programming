51da46bd73693495e6173c51887b2a5c67a0dc2d
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-14 21:06:21 +0100

    commit

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const arg = process.argv.slice(2);
-const fs = require('fs')
-let file1 = fs.readFileSync(arg[0], 'utf8')
-let file2 = fs.readFileSync(arg[1], 'utf8')
-fs.writeFileSync(arg[2], file1 + file2)
+const fs = require('fs');
+const file1 = fs.readFileSync(arg[0], 'utf8');
+const file2 = fs.readFileSync(arg[1], 'utf8');
+fs.writeFileSync(arg[2], file1 + file2);
