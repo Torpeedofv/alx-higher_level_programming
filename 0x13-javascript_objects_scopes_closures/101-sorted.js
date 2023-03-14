669669c068d4214c6bde97b6b2d17d6389955c28
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-14 17:37:57 +0100

    commit

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let newDict = {}
-for (let [key, value] of Object.entries(dict)) {
-    if (!newDict[value]) {
-        newDict[value] = [];
-    }
-    newDict[value].push(key);
-    }
-console.log(newDict)
-console.log(dict)
+const newDict = {};
+for (const [key, value] of Object.entries(dict)) {
+  if (!newDict[value]) {
+    newDict[value] = [];
+  }
+  newDict[value].push(key);
+}
+console.log(newDict);
