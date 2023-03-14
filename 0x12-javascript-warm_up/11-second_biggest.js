7f70f02a376d6b2e2f30aa53a6b41a0e24754829
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-14 08:42:03 +0100

    commit

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 // searches the second biggest in the list of integers
 const arg = process.argv.slice(2);
+
+function compare (a, b) {
+  return a - b;
+}
 if (arg.length <= 1) {
   console.log(0);
 } else {
-  function compare (a, b) {
-    return a - b;
-  }
   arg.sort(compare);
   const secBig = arg.length - 2;
   console.log(arg[secBig]);
