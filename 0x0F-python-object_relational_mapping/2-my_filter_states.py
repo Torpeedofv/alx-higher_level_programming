6cd2d107deebd0fba6625fc506437ab83d4a7462
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-19 12:04:41 +0100

    commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 """A script that takes in an argument and displays all values
 in the states table of hbtn-0e_0_usa where the name matches the argument
 """
 
+import MySQLdb
+import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
                          password=sys.argv[2], database=sys.argv[3])
