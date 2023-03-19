1e163808d02e0b5927b795dc1836645fab38df42
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-19 12:10:13 +0100

    commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""Displays all values in the states table of the database hbtn_0e_0_usa
-whose name matches that supplied as argument.
-Usage: ./2-my_filter_states.py <mysql username> \
-                               <mysql password> \
-                               <database name> \
-                               <state name searched>
+"""A script that takes in an argument and displays all values
+in the states table of hbtn-0e_0_usa where the name matches the argument
 """
 
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
-
+    db = MySQLdb.connect(user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3])
+    cur = db.cursor()
+    arg = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE\
+                name = %s ORDER BY id ASC", (arg,))
+    row = cur.fetchall()
+    [print(rows) for rows in row]
