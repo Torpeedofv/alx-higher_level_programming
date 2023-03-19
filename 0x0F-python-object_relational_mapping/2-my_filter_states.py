e5b014caeeae938ca5aedc9469bac6120a786888
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-19 12:03:42 +0100

    commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
-"""A script that takes in an argument and displays all values in the states table of hbtn-0e_0_usa where the name matches the argument
+"""A script that takes in an argument and displays all values
+in the states table of hbtn-0e_0_usa where the name matches the argument
 """
 
 if __name__ == "__main__":
@@ -9,6 +10,7 @@ if __name__ == "__main__":
                          password=sys.argv[2], database=sys.argv[3])
     cur = db.cursor()
     arg = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (arg,))
+    cur.execute("SELECT * FROM states WHERE\
+                name = %s ORDER BY id ASC", (arg,))
     row = cur.fetchall()
     [print(rows) for rows in row]
