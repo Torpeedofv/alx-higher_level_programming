5189d28b48af91ef2eb13d5237a1d6e4b8452995
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-19 13:36:17 +0100

    commit

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,6 +5,7 @@ the database hbtn_0e_4_usa"""
 
 import MySQLdb
 import sys
+
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
                          password=sys.argv[2], database=sys.argv[3])
