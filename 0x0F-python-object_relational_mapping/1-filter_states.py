7c391ed2d1798328e985da499c403d52e7a8afc8
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-19 11:53:18 +0100

    commit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+"""Write a script that lists all states with specified name from the database
+"""
 import MySQLdb
 import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
                          password=sys.argv[2], database=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states
+    cur.execute("SELECT * FROM states\
                 WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     row = cur.fetchall()
     [print(rows) for rows in row]
