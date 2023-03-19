2e83a94546691e7dfc27c3c1a50a6439e54081ae
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-19 13:38:58 +0100

    commit

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,6 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
                          password=sys.argv[2], database=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name\
+        FROM cities JOIN states ON\
+            cities.state_id=states.id ORDER BY cities.id ASC")
     row = cur.fetchall()
     [print(rows) for rows in row]
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """a script that takes in the name of a state as an
-argument and lists all cities of that state using 
+argument and lists all cities of that state using
 the database hbtn_0e_4_usa"""
 
 import MySQLdb
@@ -11,6 +11,8 @@ if __name__ == "__main__":
                          password=sys.argv[2], database=sys.argv[3])
     cur = db.cursor()
     arg = sys.argv[4]
-    cur.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id=states.id WHERE states.name= %s ORDER BY cities.id ASC", (arg,))
+    cur.execute("SELECT cities.name FROM cities JOIN\
+        states ON cities.state_id=states.id WHERE states.name= %s\
+            ORDER BY cities.id ASC", (arg,))
     row = cur.fetchall()
     print(", ".join(rowe[0] for rowe in row))
