7d7468f8f74fbdfa6313ba7d8b40a871820ff149
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-19 12:05:04 +0100

    commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,6 +5,7 @@ in the states table of hbtn-0e_0_usa where the name matches the argument
 
 import MySQLdb
 import sys
+
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
                          password=sys.argv[2], database=sys.argv[3])
