18090cb73b53aecd65216243f230e47d8231f71d
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-30 10:48:57 -0700

    sixth task redone

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a POST request and display the body of the response
-curl -sX POST -d "email=test%40gmail.com&subject=I+ will+always+be+here+for+PLD" $1
+curl -sX POST -d "email=test%40gmail.com&subject=I+will+always+be+here+for+PLD" $1
