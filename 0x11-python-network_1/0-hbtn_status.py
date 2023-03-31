71ec13e2da97a17a4c27ff0022e22f513795e17a
Torpeedofv <torpeedo.fv@gmail.com>
2023-03-31 15:40:26 +0100

    commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,10 @@ response in a specific format"""
 
 
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as body:
-    f_body = body.read()
-    print('Body response:')
-    print(f"    - type: {type(f_body)}")
-    print(f"    - content: {f_body}")
-    print(f"    - utf8 content: {f_body.decode('utf-8')}")
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as body:
+        f_body = body.read()
+        print('Body response:')
+        print(f"    - type: {type(f_body)}")
+        print(f"    - content: {f_body}")
+        print(f"    - utf8 content: {f_body.decode('utf-8')}")
