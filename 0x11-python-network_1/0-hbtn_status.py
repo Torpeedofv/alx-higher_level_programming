bfbcb3c830ec7e620dab3ffc37495bcd7a461fb5
Torpeedofv <torpeedo.fv@gmail.com>
2023-04-01 05:23:16 -0700

    Task 0 correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as body:
         f_body = body.read()
         print('Body response:')
-        print(f"    - type: {type(f_body)}")
-        print(f"    - content: {f_body}")
-        print(f"    - utf8 content: {f_body.decode('utf-8')}")
+        print(f"\t- type: {type(f_body)}")
+        print(f"\t- content: {f_body}")
+        print(f"\t- utf8 content: {f_body.decode('utf-8')}")
