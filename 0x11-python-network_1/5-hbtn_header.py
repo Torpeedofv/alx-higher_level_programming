13ac0259de06cee963c00bbfdacf03349f86ba48
Torpeedofv <torpeedo.fv@gmail.com>
2023-04-02 07:03:16 -0700

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Takes in a url, sends a request to the url and displays a specified
+variable"""
+
+
+import sys
+import requests
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
