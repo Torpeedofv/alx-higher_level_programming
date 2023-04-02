36c9982d8688910b3411b6b77b9ed24b8019ab0e
Torpeedofv <torpeedo.fv@gmail.com>
2023-04-02 14:20:50 -0700

    task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,5 +6,10 @@ takes a github username and password and uses the github API to display the id
 
 import requests
 import sys
-param = {'username': sys.argv[1], 'password': sys.argv[2]}
-
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    headers = {'Authorization': f'Basic {username}:{password}'}
+    r = requests.get('https://api.github.com/user', data=headers)
+    response = r.json()
+    print(response.get('id'))
