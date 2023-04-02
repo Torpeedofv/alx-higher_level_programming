3197883a8af4ff3d0899d981a15998753be5b535
Torpeedofv <torpeedo.fv@gmail.com>
2023-04-02 07:25:34 -0700

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Takes in a url and email address, sends a Post request to the url
+with the email as a parameter and displays the body of the response
+"""
+
+
+import requests, sys
+if __name__ == "__main__":
+    url = sys.argv[1]
+    payload = {'email': sys.argv[2]}
+    r = requests.post(url, params=payload)
+    print(r.text)
