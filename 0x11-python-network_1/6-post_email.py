ef9adf648b01e5698f851f271f7e71e6fb7fc0e5
Torpeedofv <torpeedo.fv@gmail.com>
2023-04-02 07:27:27 -0700

    task 6 correction

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,7 +4,8 @@ with the email as a parameter and displays the body of the response
 """
 
 
-import requests, sys
+import requests
+import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     payload = {'email': sys.argv[2]}
